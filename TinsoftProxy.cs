89d48c882903eb63bda3c0751e4bed2276cf3266
sonytuan <44522808+sonytuan@users.noreply.github.com>
2020-06-13 20:10:35 +0700

    Update TinsoftProxy.cs

--- TinsoftProxy.cs
+++ TinsoftProxy.cs
@@ -157,6 +157,8 @@ namespace Proxy_Client_Tinsoft
 
                 using (WebClient client = new WebClient())
                 {
+                    client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; " + 
+                                  "Windows NT 5.2; .NET CLR 1.0.3705;)");
                     rs = client.DownloadString(url);
                 }
                 if (string.IsNullOrEmpty(rs)) { rs = ""; }
